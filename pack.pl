f3445ab34dcac15b38868f4b462c30c4aea0fc24
Arnaud Nguembang Fadja <arnaud.fadja.n@gmail.com>
2024-11-05 11:30:42 +0100

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(phil).
 title('Learning Hierarchical Probabilistic Logic Programs').
-version('1.0.2').
+version('1.0.0').
 author('Nguembang Fadja Arnaud','arnaud.nguembafadja@unife.it').
 download('https://github.com/ArnaudFadja/phil/releases/*.zip').
 requires(auc).
